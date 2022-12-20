8c99a50c67cec70a2530e26f7b0e63289266ac08
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2022-12-20 12:02:17 +0100

    made some modifications

--- shell.h
+++ shell.h
@@ -1,17 +1,19 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
+/* File Headers */
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdlib.h>
 
+/* Prototypes.*/
+int _getline(void);
 
 
 
 
-
-
-#endif
+#endif /* _SHELL_H_ */
