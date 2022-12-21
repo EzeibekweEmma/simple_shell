9cad6336fc7f25e6471ac24dc99c40b1bded39de
Ezra-Mallo <ezra.mallo@gmail.com>
2022-12-21 23:23:35 +0100

    made some betty modifications.

--- 3-stat_env.c
+++ 3-stat_env.c
@@ -9,8 +9,6 @@
  */
 char *_check_stat_environ(char *buff, char **argv)
 {
-	int i = 0;
-	char *token = NULL, *key = "PATH", *path = NULL, **env_cpy = NULL;
 
 	if (strlen(argv[0]) <= 1)
 		return ("incomplete");
@@ -19,34 +17,5 @@ char *_check_stat_environ(char *buff, char **argv)
 		_execve(argv);
 		return ("done");
 	}
-	while (environ[i])
-		i++;
-	env_cpy = malloc(sizeof(char *) * i);
-	for (i = 0; environ[i]; i++)
-		env_cpy[i] = strdup(environ[i]);
-	env_cpy[i] = NULL, i = 0;
-	while (env_cpy[i])
-	{
-		token = strtok(env_cpy[i++], "=");
-		if (strcmp(key, token) == 0)
-		{
-			env_cpy = NULL;
-			break;
-		}
-	}
-	token = strtok(NULL, "\0"), token = strtok(token, ":");
-	while (token != NULL)
-	{
-		path = malloc(sizeof(char) * (strlen(token) + strlen(buff) + 1));
-		strcat(path, token), strcat(path, "/"), strcat(path, buff);
-		if (stat(path, &st) == 0)
-		{
-			argv[0] = strdup(path);
-			_execve(argv),  env_cpy = NULL,  path = NULL;
-			break;
-		}
-		token = strtok(NULL, ":");
-	}
-	env_cpy = NULL,  path = NULL, free(env_cpy), free(path);
 	return ("done");
 }
